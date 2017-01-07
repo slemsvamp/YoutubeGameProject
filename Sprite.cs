fc7d274c2cdeda9ee489a3328257c10fc6e4bc63
slemsvamp <silverljus@gmail.com>
2017-01-07 21:06:31 +0100

    01 - Making a Sprite in Monogame

---
+++ Sprite.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeGameProject {
+    public class Sprite {
+        public Vector2 Position;
+        public Texture2D Texture;
+
+        public Sprite(Texture2D pTexture, Vector2 pPosition) {
+            Position = pPosition;
+            Texture = pTexture;
+        }
+
+        public void Draw(SpriteBatch pSpriteBatch) {
+            pSpriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
--- YoutubeGame.cs
+++ YoutubeGame.cs
@@ -4,70 +4,60 @@ using Microsoft.Xna.Framework.Input;
 using System.IO;
 
 namespace YoutubeGameProject {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class YoutubeGame : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D spaceIslandTexture;
+        Sprite tinyMaleSprite;
 
         public YoutubeGame() {
             graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize() {
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent() {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             spaceIslandTexture = Texture2D.FromStream(GraphicsDevice, File.OpenRead("Content/space-island.png"));
+            Texture2D tinyMaleTexture = Texture2D.FromStream(GraphicsDevice, File.OpenRead("Content/tiny-male.png"));
+            tinyMaleSprite = new Sprite(tinyMaleTexture, new Vector2(100, 100));
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent() {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.W)) {
+                tinyMaleSprite.Position.Y -= 10 * gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            }
+            if (keyboardState.IsKeyDown(Keys.S)) {
+                tinyMaleSprite.Position.Y += 10 * gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            }
+            if (keyboardState.IsKeyDown(Keys.A)) {
+                tinyMaleSprite.Position.X -= 30 * gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            }
+            if (keyboardState.IsKeyDown(Keys.D)) {
+                tinyMaleSprite.Position.X += 30 * gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            }
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointWrap);
             spriteBatch.Draw(spaceIslandTexture, Vector2.Zero, Color.White);
+            tinyMaleSprite.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
